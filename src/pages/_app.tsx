466b9f48737dca30fe2bb0536ca4bc2d826c0172
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2022-10-15 22:00:30 -0300

    Added vlibras

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import "../../styles/globals.css";
 import type { AppProps } from "next/app";
+import VLibras from "vlibras-nextjs";
+import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} /> <VLibras forceOnload />
+    </>
+  );
 }
 
 export default MyApp;
